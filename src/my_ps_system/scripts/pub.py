3f9cdb50c36f378ecd22aae4e21b72a24d53e9e5
YahiaEldakhakhny <yahia4360673@gmail.com>
2023-12-08 12:07:46 +0000

    publisher works

--- src/my_ps_system/scripts/pub.py
+++ src/my_ps_system/scripts/pub.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 import rospy as rp
+from std_msgs.msg import String
+from time import sleep
 
 def main():
     rp.init_node("pub_tst")
     rp.loginfo("hello from pub_tst")
+
+    pub = rp.Publisher("/my_topic", String, queue_size=10)
+
     while not rp.is_shutdown():
-        rp.sleep(1.0)
-        rp.loginfo("IT'S ALIVE")
-    rp.loginfo("End of node")
+        hello_str = f"hello at time {rp.get_time()}"
+        rp.loginfo(hello_str)
+        pub.publish(hello_str)
+        sleep(0.5)
+
+
 
 
 if __name__ == "__main__":
